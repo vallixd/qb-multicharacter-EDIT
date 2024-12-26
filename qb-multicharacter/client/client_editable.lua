b163eb04c9fab1db390bcf714f25c2713e7e4bc5
VL <zikobedirhan98@gmail.com>
2024-12-26 17:38:30 +0300

    Delete qb-multicharacter directory

--- qb-multicharacter/client/client_editable.lua
+++
@@ -1,9 +0,0 @@
-QBCore = nil
-
-local function InitializeFrameworkDependentComponents()
-    QBCore = exports['qb-core']:GetCoreObject()
-end
-
-CreateThread(function()
-    InitializeFrameworkDependentComponents()
-end)
